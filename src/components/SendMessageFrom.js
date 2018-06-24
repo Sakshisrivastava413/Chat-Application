91139393baba24c80b3114d947c75e58fda8bf3b
Sakshi <sakshisrivastava413@gmail.com>
2018-06-24 18:37:50 +0530

    added component to send messages

--- src/components/SendMessageFrom.js
+++ src/components/SendMessageFrom.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
 
 export default class SendMessageFrom extends Component {
+  constructor() {
+    super()
+    this.state = {
+      message: ''
+    }
+  }
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.message)
+  }
   render() {
     return (
       <div>
-        
+        <form onSubmit={this.handleSubmit} className="send-message-form">
+          <input
+            onChange={this.handleChange}
+            value={this.state.message}
+            placeholder="Type your message and hit ENTER"
+            type="text"
+          />
+        </form>
       </div>
     )
   }
